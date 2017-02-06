7d64cb27be5c4c1a6388b83ef53940fb5290b88b
BoTanCHAOSNETS <uceebta@ucl.ac.uk>
2017-02-06 21:18:27 +0000

    Desribption of Mapping algporithm

---
+++ Mapping.m
@@ -0,0 +1,23 @@
+clear all;
+
+
+%%%% Define the Map
+% X range 
+% Y range 
+% Grid Size
+
+
+%%%% Determine the Reflection Spot
+% 16 spots, 24 angles on each spot, each angle cancel 8 layers, 16*24*8 = 3072 JADEs
+% This part calculates reflection location of each JADE suface 
+
+
+%%%% Moving Shift vector
+% x-y coordination shift of each measurement spot
+% orientation of each measurement spot
+% need to think about how to synthesis the orientation information to each JADE surface
+
+
+%%%% Synthesis Information
+% extend the moving shift vector to 3072 size
+% synthesis the map
