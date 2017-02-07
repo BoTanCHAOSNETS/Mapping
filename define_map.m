5724108f54e4f2c54d39489a55744e823f40ade6
BoTanCHAOSNETS <uceebta@ucl.ac.uk>
2017-02-07 23:21:45 +0000

    defined the basic sturcture

--- define_map.m
+++ define_map.m
@@ -1,1 +1,14 @@
-function [] = define_map()
+function [layout_grid] = define_map(x_range,y_range,resolution)
+
+x_vector = [1 : resolution : x_range];
+y_vector = [1 : resolution : y_range];
+
+grid_size = numel(x_vector);
+layout_grid = cell(grid_size);
+
+initial_intensity = 0;
+for k = 1:grid_size
+    for h = 1:grid_size
+        layout_grid{k,h} = [x_vector(k), x_vector(h), initial_intensity];
+    end
+end
--- determine_reflection_location.m
+++ determine_reflection_location.m
@@ -1,1 +1,9 @@
-function [] = determine_reflection_location()
+function [relative_location] = determine_reflection_location(jade_surface,spot_index,angle_index,cancel_index)
+
+[r,c] = find(jade_surface == max(jade_surface(:)));
+
+% convert JADE to x-y 
+%
+
+relative_location{k} = [x_position,y_position,spot_index,angle_index,cancel_index];
+
